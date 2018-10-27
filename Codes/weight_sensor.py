cb453af165354309606ada68b375fbe2d6a5a26a
CamilaFerrer <camila.c.ferrer@gmail.com>
2018-10-27 00:08:36 +0000

    Código de medição de peso da lixeira

---
+++ Codes/weight_sensor.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+from hx711 import HX711
+
+def cleanAndExit():
+    print "Limpando GPIO..."
+    GPIO.cleanup()
+    print "Fim!"
+    sys.exit()
+
+hx = HX711(5, 6)
+hx.set_reading_format("LSB", "MSB")
+hx.set_reference_unit(92)
+
+hx.reset()
+hx.tare()
+
+while True:
+    try:
+        val = hx.get_weight(5)
+        print val
+
+        hx.power_down()
+        hx.power_up()
+        time.sleep(0.5)
+    except (KeyboardInterrupt, SystemExit):
+        cleanAndExit()
