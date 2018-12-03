500959dcf5b2ffeadfad3fe9c3cf75235bb68f6b
CamilaFerrer <camila.c.ferrer@gmail.com>
2018-12-03 18:31:17 -0200

    Atualização Acelerômetro

--- Codes/acegyrotemp.py
+++ Codes/acegyrotemp.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from mpu6050 import mpu6050
 
 sensor = mpu6050(0x68)
@@ -7,7 +9,9 @@ gyro = sensor.get_gyro_data()
 temp = sensor.get_temp()
 
 
-print("Ax: {a_x} m/s² | Ay: {a_y} m/s² |Az: {a_z} m/s²".format(a_x=round(accel[0],1),a_y=round(accel[1],1), a_z=round(accel[2],1))
-print("Gx: {g_x} º    | Gy: {g_y} º    |Gz: {g_z} º".format(g_x=round(gyro[0],0),g_y=round(gyro[1],0), g_z=round(gyro[2],0))
-print("Temperatira: {temp} ºC".format(temp=round(temp,1)))
+print("Ax: {a_x} m/s²".format(a_x=round(accel['x'],1)))
+print("Ay: {a_y} m/s²".format(a_y=round(accel['y'],1)))
+print("Gx: {g_x} °".format(g_x=round(gyro['x'],0)))
+print("Gy: {g_y} °".format(g_y=round(gyro['y'],0)))
+print("Temperatira: {temp} °C".format(temp=round(temp,1)))
 
